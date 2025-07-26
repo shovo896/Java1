9263caabbef29b49e35976fd64b7b1781faa1ccf
Shuvo <suvo10.info@gmail.com>
2025-07-26 14:17:54 +0600

    initial

---
+++ sort.java
@@ -0,0 +1,10 @@
+public class sort {
+    public static void main(String[] args) {
+        // This class is used to sort the files based on their content
+       short num=1000;
+        short t = -200;
+        System.out.println("Number of students :" +num) ;
+        System.out.println("Number of teachers :" +t);  
+    
+    }
+}
