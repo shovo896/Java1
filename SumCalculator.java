1b96da7afa3014ec4a1ee3484a1e288d2e137995
Shuvo <suvo10.info@gmail.com>
2025-07-26 13:33:30 +0600

    new

---
+++ SumCalculator.java
@@ -0,0 +1,9 @@
+public class SumCalculator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+    
+}
