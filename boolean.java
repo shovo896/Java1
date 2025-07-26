4ddd106f72b539699435bc7670f17ba4c6684ca1
Shuvo <suvo10.info@gmail.com>
2025-07-26 14:13:35 +0600

    true false

---
+++ boolean.java
@@ -0,0 +1,9 @@
+public class boolean {
+    public static void main(String[] args) {
+        boolean  b1= true;
+        boolean  b2 = false;
+        System.out.println("isTrue: " +b1 );
+        System.out.println("isFalse: " +b2 );
+    }   
+    
+}
