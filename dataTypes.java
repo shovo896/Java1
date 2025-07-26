c6346fa81a24da949357b36f7741e113aa65f50c
Shuvo <suvo10.info@gmail.com>
2025-07-26 14:25:39 +0600

    meau

---
+++ dataTypes.java
@@ -0,0 +1,14 @@
+public class sort {
+    public static void main(String[] args) {
+        // This class is used to sort the files based on their content
+        long num=1000L;
+        short t = -200;
+        float pi =3.14f;
+        float gravity= 9.8f ;
+        System.out.println("Number of students :" +num) ;
+        System.out.println("Number of teachers :" +t);  
+        System.out.println(pi);
+        System.out.println(gravity);
+    
+    }
+}
