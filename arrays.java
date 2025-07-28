b1938bf8330a4da852b984cb5572e9b042484434
Shuvo <suvo10.info@gmail.com>
2025-07-28 21:30:10 +0600

    init

---
+++ arrays.java
@@ -0,0 +1,11 @@
+public class arrays {
+    public static void main(String[] args) {
+       int[] arr ={1,2,3,4,5};
+       int n = arr.length;
+       // traversing array
+       for(int i = 0; i < n; i++) 
+              System.out.print(arr[i] + " ");
+    }
+}
+    
+
