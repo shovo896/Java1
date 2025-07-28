a85ef9688d6ae7202f77837f2e9b9a829f2c2e3f
Shuvo <suvo10.info@gmail.com>
2025-07-28 21:37:04 +0600

    access

---
+++ arrayalloc.java
@@ -0,0 +1,18 @@
+public class arrayalloc {
+    public static void main(String[] args)
+    {
+        int [] arr ;
+        arr=new int[5];
+        arr[0]=10;
+        arr[1]=20;  
+        arr[2]=30;
+        arr[3]=40;
+        arr[4]=50;
+        //  accessing array elements
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
