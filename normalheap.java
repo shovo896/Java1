4a0b2f44ec5a85c8053f1206a90d6da5547cafa6
Shuvo <suvo10.info@gmail.com>
2025-07-30 12:15:40 +0600

    init

---
+++ normalheap.java
@@ -0,0 +1,14 @@
+public class normalheap {
+    public static void main(String[] args) {
+       String s1="Hello";
+       String s2="Helte helte pore gelam to ";
+       String s3="Amar buke pore jau";
+       String s4="Prem  e mora jole dobe na ";
+       System.out.println(s1);
+       System.out.println(s2);
+       System.out.println(s3);
+       System.out.println(s4);
+
+    }
+    
+}
