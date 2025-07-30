dacfe98029a74050565659c7ae0c1b706751bb5d
Shuvo <suvo10.info@gmail.com>
2025-07-30 11:11:26 +0600

    init

---
+++ meautie.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.lang.*;
+
+
+
+class meautie {
+    public static void main(String[]args){
+        String name="I am Dutee .I am a Bilai and meau .I am so cute ";
+        System.out.println("String name="+name);
+        String newString=new String("Apnar meye re biye diye sasuri hon aunty");
+        System.out.println("New String="+newString);
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
