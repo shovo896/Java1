beb4b2dca1d40260633b56797c5781a3f5fbacff
Shuvo <suvo10.info@gmail.com>
2025-07-30 12:25:34 +0600

    cutie

---
+++ pattern.java
@@ -0,0 +1,11 @@
+import java.util.regex.Pattern;
+
+
+
+
+public class pattern {
+    public static void main(String[] args) {
+        System.out.println(Pattern.matches("Dutee","cholo bbiye kori"));
+        System.out.println(Pattern.matches("Dutee","Dutee"));
+}
+}
