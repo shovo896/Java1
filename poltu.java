4c9c2f79338d89744fc424e3e9323eeb0f41124b
Shuvo <suvo10.info@gmail.com>
2025-07-30 10:47:24 +0600

    initial

---
+++ poltu.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+
+class poltu {
+    public static void main(String[] args) {
+        String s=" Schain";
+        s.concat("Tendulkar");
+        System.out.println(s);
+        // Additional functionality can be added here
+    }
+    
+}
