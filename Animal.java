4f0412241f053d86f6673b9cee26c8df11eb90d3
Shuvo <suvo10.info@gmail.com>
2025-07-31 23:54:14 +0600

    init

---
+++ Animal.java
@@ -0,0 +1,34 @@
+public class Animal {
+    private String name;
+    private int age;
+
+    // Constructor
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Instance method
+    public void display() {
+        System.out.println("Animal Name: " + name);
+        System.out.println("Animal Age: " + age);
+    }
+}
