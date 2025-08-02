3ad7d0a955692659d6e3cd0b91feb7fe572337ef
Shuvo <suvo10.info@gmail.com>
2025-08-02 18:40:11 +0600

    init

---
+++ static1.java
@@ -0,0 +1,14 @@
+interface static1 {
+    final int a = 10;
+
+    static void display() {
+        System.out.println("Static method in static1 interface");
+    }
+}
+
+// A class that implements the static interface
+class StaticClass implements static1 {
+    public static void main(String[] args) {
+        static1.display(); 
+    }
+}
