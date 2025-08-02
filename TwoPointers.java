318fc0a4364ab286c25c421222d4adb11fab1e51
Shuvo <suvo10.info@gmail.com>
2025-08-02 16:10:09 +0600

    init

---
+++ TwoPointers.java
@@ -0,0 +1,13 @@
+package JavaPrograming.arrays;
+
+
+public class TwoPointers {
+
+    public static void main(String[] args) {
+        System.out.println("Insde the package");
+        
+    }
+
+
+    
+}
---
+++ classutil.java
@@ -0,0 +1,23 @@
+import java.util.Vector;
+
+public class classutil {   
+    public classutil(){
+        Vector v=new Vector();
+        java.util.ArrayList l = new java.util.ArrayList();
+        l.add(3);
+        l.add(4);
+        l.add(5);
+        System.out.println(l);
+
+
+
+
+    } 
+    public static void main(String[] args) {
+        new classutil();
+    
+}
+}
+   
+    
+
