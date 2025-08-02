5719b853acaada5c806f77a26dec67b956c26cde
Shuvo <suvo10.info@gmail.com>
2025-08-02 16:24:52 +0600

    init

---
+++ create_package.java
@@ -0,0 +1,13 @@
+
+
+package Create_package;
+
+public class create_package {
+    public static void main(String[] args) {
+       String s ="Hello World";
+       create_package o = new create_package();
+       o.getNames(s);
+    }
+
+    
+}
