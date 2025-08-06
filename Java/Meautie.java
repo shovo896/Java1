a167543e92709308d525e2047db9adbc50d4373c
Shuvo <suvo10.info@gmail.com>
2025-08-06 23:44:30 +0600

    array list

---
+++ Java/Meautie.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+
+public class Meautie {
+    public static void main(String[]args){
+        ArrayList<String> al = new ArrayList<>();
+        al.add("Red");
+        al.add("Green");
+        al.add("Blue");
+        System.out.println("Colors in the ArrayList: " + al);
+    }
+    
+}
