f44d484a5dcb512e2fe1be076f841a49a6676387
Shuvo <suvo10.info@gmail.com>
2025-08-09 23:38:45 +0600

    collection

---
+++ Java/colec.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+
+
+public class colec {
+    public static void main(String[] args){
+        Collection<String> c = new ArrayList<>();
+        c.add("Red");
+        c.add("Green");
+        c.add("Blue");
+        c.add("Black");
+        System.out.println("Original Collection: " + c);
+          
+
+
+
+
+
+
+
+
+
+    }
+    
+}
