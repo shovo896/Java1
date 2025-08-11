7f0055a5c995ab4fbf6d5d59c119700d5f5ae4b1
Shuvo <suvo10.info@gmail.com>
2025-08-11 23:49:30 +0600

    init

---
+++ Java/binarysearchlist.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class binarysearchlist {
+    private static boolean isElementPresent(int[]arr,int key){
+        boolean res=Arrays.asList(arr).contains(key);
+        return res; // Element found or not found
+
+        }
+        public static void main(String[]args){
+            int arr[] = {1, 2, 3, 4, 5};
+            int key = 3;
+            boolean result = isElementPresent(arr, key);
+            System.out.println("Is the element " + key + " present in the array? " + result);
+        }
+    
+}
