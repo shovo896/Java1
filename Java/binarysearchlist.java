f75731322249ae56c6b42cc95459fdd4c3ae4ca8
Shuvo <suvo10.info@gmail.com>
2025-08-11 23:58:04 +0600

    change

--- Java/binarysearchlist.java
+++ Java/binarysearchlist.java
@@ -1,17 +1,21 @@
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
-class binarysearchlist {
-    private static boolean isElementPresent(Integer[]arr,int key){
-        boolean res=Arrays.asList(arr).contains(key);
-        return res; // Element found or not found
+class BinarySearchList {
+    private static boolean isElementPresent(int[] arr, int key) {
+        // Loop through the primitive array manually
+        for (int num : arr) {
+            if (num == key) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        }
-        public static void main(String[]args){
-            int arr[] = {1, 2, 3, 4, 5};
-            int key = 8;
-            boolean result = isElementPresent(arr, key);
-            System.out.println("Is the element " + key + " present in the array? " + result);
-        }
-    
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        int key = 8;
+        boolean result = isElementPresent(arr, key);
+        System.out.println("Is the element " + key + " present in the array? " + result);
+    }
 }
