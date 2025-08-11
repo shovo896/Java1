cb260cfee6f955e46fe127d3509f85fa6e80a1e7
Shuvo <suvo10.info@gmail.com>
2025-08-11 22:32:09 +0600

    error

---
+++ Java/my thread.java
@@ -0,0 +1,19 @@
+ class my thread extends Thread {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+
+    public static void main(String[] args) {
+        my thread t = new my thread();
+        t.start(); // start the thread
+    }
+
+    
+}
+class GFG {
+    public static void main(String[]args){
+        |my thread t = new my thread();
+        obj1.start(); // start the thread
+        obj2.start();
+    }
+}
