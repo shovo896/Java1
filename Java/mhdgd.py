117e14e7b49bbb411b7d210e5c303d5263c99a74
Shuvo <suvo10.info@gmail.com>
2025-08-13 00:26:04 +0600

    imdnn

--- Java/mhdgd.py
+++ Java/mhdgd.py
@@ -1,12 +1,12 @@
 rows=int(input('Enter the number of rows:'))
 for i in range(rows):
-    print(""*(rows-i),ends="")
+    print(""*(rows-i),end="")
     coef=1 
     for j in range(i+1):
         if j==0 or i ==0:
             coef=1
         else:
             coef=coef*(i-j-1)//j
-        print(f"{coef:P4}",end="")
+        print(f"{coef:4}",end="")
     print()
 
