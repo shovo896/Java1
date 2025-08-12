0964a62dd8df9eceeff884d751ea82d4e14e14a8
Shuvo <suvo10.info@gmail.com>
2025-08-12 23:19:18 +0600

    num

---
+++ Java/numpyramid.java
@@ -0,0 +1,19 @@
+public class numpyramid {
+    public static void main(String[] args){
+        int rows = 0;
+        for(int i = 0;i<=rows;++i){
+            for(int j= 0 ;j<=i;++j){
+                System.out.print(j+"");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+    }
+    
+}
