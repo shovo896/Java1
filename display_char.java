eaf726706baab86a3cce88760f420dbac95ea6dc
Shuvo <suvo10.info@gmail.com>
2025-08-13 23:19:04 +0600

    chang

--- display_char.java
+++ display_char.java
@@ -2,7 +2,7 @@
     public static void main(String[]args){
         char c ;
         for(c='A';c<='Z';++c){
-            System.out.print(c+"");
+            System.out.println(c+"");
         }
     }
 
