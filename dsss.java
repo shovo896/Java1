462930715bcb0ae5e745746c0ab04d14277f3915
Shuvo <suvo10.info@gmail.com>
2025-08-13 23:25:04 +0600

    GCD

---
+++ dsss.java
@@ -0,0 +1,17 @@
+class dsss {
+    public static void main(String[] args ){
+        int n1 =81,n2=153;
+        while(n1 != n2){
+            if (n1>n2){
+                n1-=n2;
+
+            }
+            else{
+                n2-=n1;
+            }
+        }
+        System.out.println("GCD"+n1);
+
+    }
+    
+}
